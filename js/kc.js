9f96ee7c3963186bfed12b0c2292500f448bd390
Will English <wenglish0223@gmail.com>
2018-04-26 14:52:00 -0400

    Update kc.js

--- js/kc.js
+++ js/kc.js
@@ -1,19 +1,15 @@
-(function ($) {
-    $(document).ready(function() {
         var keys = [];
         var konami = '38,38,40,40,37,39,37,39,66,65';
-        $(document)
+        (document)
             .keydown(
                 function(e) {
                     keys.push(e.keyCode);
                     if (keys.toString().indexOf(konami) >= 0) {
                         // do something when the konami code is executed
-                          $window.location.replace("http://stackoverflow.com");
-                          $console.log("sdsdfs");
+                          window.location.replace("http://stackoverflow.com");
+                          console.log("sdsdfs");
                          // empty the array containing the key sequence entered by the user
                         keys = [];
                     }
                 }
-            );
-    });
-})
+          
